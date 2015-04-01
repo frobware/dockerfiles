0cab19077891dada02c18b805e6b78355d49f590
Andrew McDermott <aim@frobware.com>
2015-04-01 12:22:40 +0100

    go-pkg: make update-alternatives install into /usr/bin

--- go-pkg/build-package.bash
+++ go-pkg/build-package.bash
@@ -7,26 +7,32 @@ set -o nounset
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 source $TOP_DIR/meta.sh
 
+priority=$(${TOP_DIR}/alternatives-priority $GO_VERSION)
+
 cat <<EOF > $TOP_DIR/after-install.bash
 #!/bin/bash
 EOF
-for i in $TOP_DIR/go/bin/*; do
+for i in $INSTALL_PREFIX/$GO_VERSION/bin/*; do
     fname=$(basename $i)
-    echo "update-alternatives --install ${INSTALL_PREFIX}/bin/$fname $fname $GOROOT_FINAL/bin/$fname 100" >> $TOP_DIR/after-install.bash
+    echo "update-alternatives --install /usr/bin/$fname $fname ${INSTALL_PREFIX}/${GO_VERSION}/bin/$fname $priority" >> $TOP_DIR/after-install.bash
 done
 
 cat <<EOF > $TOP_DIR/after-remove.bash
 #!/bin/bash
 EOF
-for i in $TOP_DIR/gi/bin/*; do
-    echo "update-alternatives --remove $fname ${INSTALL_PREFIX}/bin/$fname" >> $TOP_DIR/after-remove.bash
+for i in $INSTALL_PREFIX/$GO_VERSION/bin/*; do
+    fname=$(basename $i)
+    echo "update-alternatives --remove $fname /usr/bin/$fname" >> $TOP_DIR/after-remove.bash
 done
 
 chmod 755 $TOP_DIR/after-install.bash
 chmod 755 $TOP_DIR/after-remove.bash
 
+cat $TOP_DIR/after-install.bash
+cat $TOP_DIR/after-remove.bash
+
 for i in deb; do
-    fpm -f --prefix=$GOROOT_FINAL \
+    fpm -f --prefix=${INSTALL_PREFIX}/${GO_VERSION} \
 	-s dir \
 	-t $i \
 	-C ${INSTALL_PREFIX}/${GO_VERSION} \
@@ -41,7 +47,7 @@ done
 fpm -f --prefix=$GO_VERSION \
     -s dir \
     -t tar \
-    -C ${GO_VERSION} \
+    -C ${INSTALL_PREFIX}/${GO_VERSION} \
     --name "${GO_VERSION}" \
     --version 1.0.0 \
     --description "Go ${GO_VERSION}" \
--- go-pkg/meta.sh
+++ go-pkg/meta.sh
@@ -1,11 +1,10 @@
 set -a
 
-: ${PKG_ITERATION:=4}
+: ${PKG_ITERATION:=7}
 : ${GO_VERSION:=go1.4.2}
 : ${INSTALL_PREFIX:=/usr/local}
-: ${GOROOT_FINAL:=${INSTALL_PREFIX}/${GO_VERSION}}
 : ${GOPATH:=/work}
 
-PATH=$GOROOT_FINAL/bin:$PATH
+PATH=$INSTALL_PREFIX/$GO_VERSION/bin:$PATH
 
 set +a
